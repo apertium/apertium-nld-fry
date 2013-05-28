4f36dac124b400575ae374de805bc69a993f8868
Francis M. Tyers <ftyers@prompsit.com>
2013-05-28 19:57:22 +0000

    more

--- dev/testvoc/inconsistency.sh
+++ dev/testvoc/inconsistency.sh
@@ -4,7 +4,7 @@ DIR=$1
 
 if [[ $DIR = "fry-nld" ]]; then
 
-lt-expand ../../apertium-nld-fry.fry.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
+lt-expand ../../apertium-nld-fry.fry.dix | grep -v -e '<compound-' -e 'DUE_TO_LT_PROC_HANG' | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
         apertium-pretransfer|
 	lt-proc -b ../../fry-nld.autobil.bin |  tee $TMPDIR/$DIR.tmp_testvoc2.txt |
         apertium-transfer -b ../../apertium-nld-fry.fry-nld.t1x  ../../fry-nld.t1x.bin | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
@@ -13,7 +13,7 @@ paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$
 
 elif [[ $DIR = "nld-fry" ]]; then
 
-lt-expand ../../apertium-nld-fry.nld.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
+lt-expand ../../apertium-nld-fry.nld.dix | grep -v -e '<compound-' -e 'DUE_TO_LT_PROC_HANG' | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
         apertium-pretransfer|
 	lt-proc -b ../../nld-fry.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |
         apertium-transfer -b ../../apertium-nld-fry.nld-fry.t1x  ../../nld-fry.t1x.bin | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
